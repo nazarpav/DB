f867c11d0a004b90f9dc4b4b86b741d9689c9952
павлюк назар <hasab.k18@gmail.com>
2020-01-23 01:29:18 +0200

    23.01.2020

--- HomeWork/02.01.2020/Создание базы данных.sql
+++ HomeWork/02.01.2020/Создание базы данных.sql
@@ -80,8 +80,31 @@
 --('W1',3,1),
 --('W2',11,1),
 --('W3',13,2),
---('W4',6,2)
-
+--('W4',6,2),
+--('W5',3,1)
+--GO
+--INSERT Doctors
+--VALUES
+--('ROB',100,1000,'KOLOBKO'),
+--('ALEX',50,900,'GRICH'),
+--('ADAN',150,1500,'Smith'),
+--('GUNNY',200,1800,'Williams'),
+--('JADA',125,800,'Miller')
+--GO
+--INSERT Examinations
+--VALUES
+--('E1'),
+--('E2'),
+--('E3'),
+--('E4')
+--GO
+--INSERT DoctorsExaminations
+--VALUES
+--('20:00','10:00',1,1,1),
+--('20:00','10:00',2,2,1),
+--('20:00','10:00',3,3,2),
+--('20:00','10:00',4,3,3),
+--('20:00','10:00',5,4,4)
 ----------------------------------------------------------------------------------------------
 
 ------------------TASK_1
@@ -91,18 +114,70 @@
 --WHERE Places>10
 --------------------TASK_2
 --GO
---SELECT Departments.Building,COUNT(Departments.Id) 
---FROM Departments join Wards on Wards.DepartmentId = Departments.Id
+--SELECT Departments.Building,COUNT(Wards.Places) 
+--FROM Departments 
+--INNER JOIN Wards ON Wards.DepartmentId = Departments.Id
 --GROUP BY Departments.Building
 
 ---------------------TASK_3
 --GO
 --SELECT Departments.[Name],COUNT(Departments.Id) 
---FROM Departments join Wards on Wards.DepartmentId = Departments.Id
+--FROM Departments 
+--INNER JOIN Wards ON Wards.DepartmentId = Departments.Id
 --GROUP BY Departments.[Name]
 
 ------------------------TASK_4
-GO
-SELECT Departments.[Name],COUNT(Departments.Id) 
-FROM Departments join Wards on Wards.DepartmentId = Departments.Id
-GROUP BY Departments.[Name]
+--GO
+--SELECT Departments.[Name],SUM(Doctors.Premium) 
+--FROM Departments
+--INNER JOIN Wards ON Wards.DepartmentId = Departments.Id
+--INNER JOIN DoctorsExaminations ON Wards.Id = DoctorsExaminations.WardId
+--INNER JOIN Doctors ON DoctorsExaminations.DoctorId=Doctors.Id
+--GROUP BY Departments.[Name]
+
+----------------------TASK_5
+--GO
+--SELECT Departments.[Name]
+--FROM Departments
+--INNER JOIN Wards ON Wards.DepartmentId = Departments.Id
+--INNER JOIN DoctorsExaminations ON Wards.Id = DoctorsExaminations.WardId
+--GROUP BY Departments.[Name]
+--HAVING COUNT(DoctorsExaminations.DoctorId)>2
+
+-------------------------TASK_6
+--GO
+--SELECT COUNT(Id),SUM(Salary)+SUM(Premium)
+--FROM Doctors
+
+---------------------------TASK_7
+--GO
+--SELECT SUM(Salary)+SUM(Premium)/COUNT(Id)
+--FROM Doctors
+
+----------------------------TASK_8
+--GO
+--SELECT w.[Name]
+--FROM Wards AS w
+--WHERE  w.Places =  (SELECT MIN(w.places) FROM Wards AS w)
+--GROUP BY w.[Name]
+--GO
+--SELECT [Name]
+--FROM Wards
+--WHERE Places=MIN(Places)
+----GROUP BY [Name]
+----HAVING Places=MIN(Places)
+
+------------------------------TASK_9
+--GO
+--SELECT Departments.Building,SUM(Wards.Places)
+--FROM Departments 
+--INNER JOIN Wards ON Wards.DepartmentId = Departments.Id
+--WHERE Wards.Places>5 AND
+--				(
+--				Departments.Building=1
+--				OR Departments.Building=2
+--				OR Departments.Building=7
+--				OR Departments.Building=8
+--				)
+--GROUP BY Departments.Building
+--HAVING SUM(Wards.Places)>10
