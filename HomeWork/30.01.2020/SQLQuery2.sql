0485ce2fd808553c171056de8bfff632d2830e04
павлюк назар <hasab.k18@gmail.com>
2020-01-31 02:16:55 +0200

    31.01.2020

--- HomeWork/30.01.2020/SQLQuery2.sql
+++ HomeWork/30.01.2020/SQLQuery2.sql
@@ -1,2 +1,25 @@
-GO
-Ÿ≈ œ–¿÷ﬁﬁ Õ¿ƒ ÷»Ã «¿¬ƒ¿ÕÕﬂÃ
+-------TASK_1
+--GO
+--ALTER TABLE Customers
+--DROP CONSTRAINT PK__Customer__3214EC071920BF5C
+--GO
+--CREATE CLUSTERED INDEX CustomersEmail ON Customers(Email)
+
+--------------TASK_2
+--GO
+--CREATE INDEX Archive ON Archive([Date])
+
+--GO
+--CREATE INDEX Sales ON Sales([Customer Email])
+
+----------------TASK_4
+--GO
+--CREATE UNIQUE INDEX T_4
+--ON History([Customer Email])
+--INCLUDE([Sale Price],[Sale Date]);
+
+---------------------TASK_5
+--GO
+--CREATE NONCLUSTERED INDEX T_5
+--ON Employees (Position)
+--WHERE Position='Consultant';
